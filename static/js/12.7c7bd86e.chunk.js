baaf54672a5da8ea821b72b437221a9bb24b5185
Ari Perkkio <ari.perkkio@gmail.com>
2020-05-29 20:47:58 +0300

    Updates

--- static/js/12.7c7bd86e.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{296:function(n,o,w){"use strict";w.r(o),o.default=function(){return"Mobx view"}}}]);
---
+++ static/js/14.e4247b33.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{315:function(n,o,w){"use strict";w.r(o),o.default=function(){return"Mobx view"}}}]);
